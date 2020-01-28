683e298fe543fea47118a11d8d09ef4f7b6c3894
Liz Laffitte <liz.kossakowski@gmail.com>
2020-01-28 02:20:49 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -5,7 +5,7 @@ class Ship
     def initialize(params)
         @name = params[:name]
         @type = params[:type]
-        @booty = parms[:booty]
+        @booty = params[:booty]
         @@all << self
     end
 
